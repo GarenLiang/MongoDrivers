797e52edb44c676cda7220601667e831864b7388
GarenLiang <ideabreeze@gmail.com>
2017-04-09 18:02:16 -0700

    Geo Near function

--- models/driver.js
+++ models/driver.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 
 const PointSchema = new Schema({
+
   type: { type: String, default: 'Point' },
   coordinates: { type: [Number], index: '2dsphere' }
 });
@@ -15,7 +16,7 @@ const DriverSchema = new Schema({
     type: Boolean,
     default: false
   },
-  geometry: PointSchema;
+  geometry: PointSchema
 });
 
 const Driver = mongoose.model('driver', DriverSchema);
